0bf2cd67a9280e786c2412fc1027d99d762b88fc
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2026-02-19 04:36:43 +0500

    Прописаны тесты

--- src/components/form-register/FormRegister.test.tsx
+++ src/components/form-register/FormRegister.test.tsx
@@ -1,17 +1,14 @@
 import { screen, render, waitFor, cleanup } from "@testing-library/react";
-import { vi, it, describe, expect, afterEach } from "vitest";
+import { it, describe, expect, afterEach } from "vitest";
 import { FormRegister } from "./FormRegister";
 import userEvent from "@testing-library/user-event";
 
 describe("Компонент Form", () => {
   afterEach(cleanup);
 
-  it("Должен показать ошибку при некорректном заполнении поля ФИО (неполное имя)", async () => {
+  it("Должен показать ошибку при некорректном заполнении поля ФИО", async () => {
     const user = userEvent.setup();
     render(<FormRegister />);
-    // const result = render(<FormRegister />);
-
-    // screen.debug();
 
     const nameInput = screen.getByRole("textbox", { name: /фио/i });
     const submitButton = screen.getByRole("button", { name: /записаться/i });
@@ -25,4 +22,36 @@ describe("Компонент Form", () => {
       ).toBeDefined();
     });
   });
+
+  it("Должен показать ошибку при некорректном заполнении поля возраст", async () => {
+    const user = userEvent.setup();
+    render(<FormRegister />);
+
+    const nameInput = screen.getByRole("textbox", { name: /фио/i });
+    const ageInput = screen.getByRole("spinbutton", { name: /возраст/i });
+    const submitButton = screen.getByRole("button", { name: /записаться/i });
+
+    await user.type(nameInput, "Иванов Иван Иванович");
+    await user.type(ageInput, "150"); 
+    await user.click(submitButton);
+
+    expect(screen.getByText(/должно быть в диапазоне от 1 до 100/i)).toBeInTheDocument();
+  });
+
+  it("Должна успешно отправляться форма", async () => {
+    const user = userEvent.setup();
+    render(<FormRegister />);
+
+    const nameInput = screen.getByRole("textbox", { name: /фио/i });
+    const ageInput = screen.getByRole("spinbutton", { name: /возраст/i });
+    const submitButton = screen.getByRole("button", { name: /записаться/i });
+
+    await user.type(nameInput, "Иванов Иван Иванович");
+    await user.type(ageInput, "20");
+    await user.click(submitButton);
+
+    expect(screen.getByText(/Ваша заявка отправлена/i)).toBeInTheDocument();
+
+    expect(nameInput).toHaveValue("");
+  })
 });
