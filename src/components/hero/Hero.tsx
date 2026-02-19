0f3fd1b44df6bb199635e867b348ae4f4eb996a8
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2026-02-19 21:17:51 +0500

    Сделана прокрутка на колесико в hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,13 +1,38 @@
+"use client";
+
 import { Container } from "@/shared/ui";
 import { HeroList } from "./ui/HeroList";
 import { MUGS_ITEMS } from "@/constants/mugs-items";
+import { useEffect, useRef } from "react";
 
 export const Hero = () => {
+  const scrollRef = useRef<HTMLUListElement | null>(null);
+
+  useEffect(() => {
+    const el = scrollRef.current;
+    if (!el) return;
+
+    const onWheel = (e: globalThis.WheelEvent) => {
+      if (e.deltaY === 0) return;
+
+      e.preventDefault();
+
+      el.scrollTo({
+        left: el.scrollLeft + e.deltaY * 2, 
+        behavior: "smooth",
+      });
+    };
+
+    el.addEventListener("wheel", onWheel, { passive: false });
+
+    return () => el.removeEventListener("wheel", onWheel);
+  }, []);
+
   return (
     <section id="mugs" className="py-[clamp(65px,12vw,120px)] scroll-mt-[30vh]">
       <Container>
-        <HeroList mugsItems={MUGS_ITEMS}/>
+        <HeroList mugsItems={MUGS_ITEMS} scrollRef={scrollRef} />
       </Container>
     </section>
   );
-}
+};
--- src/components/hero/ui/HeroList.tsx
+++ src/components/hero/ui/HeroList.tsx
@@ -1,16 +1,24 @@
 import { IMugsItems } from "@/constants/mugs-items";
 import { HeroListItem } from "./HeroListItem";
+import { RefObject } from "react";
 
 interface IHeroListProps {
   mugsItems: IMugsItems[];
+  scrollRef: RefObject<HTMLUListElement | null>;
 }
-  
-export const HeroList = ({ mugsItems }: IHeroListProps) => {
+
+export const HeroList = ({
+  mugsItems,
+  scrollRef,
+}: IHeroListProps) => {
   return (
-    <ul className={`flex h-100 py-5 w-full gap-x-[clamp(24px,8vw,53px)] overflow-auto md:scrollbar-hide snap-x snap-mandatory`}>
-      { mugsItems.map((item) => (
+    <ul
+      ref={scrollRef}
+      className={`flex flex-nowrap h-100 py-5 w-full gap-x-[clamp(24px,8vw,53px)] overflow-auto md:scrollbar-hide snap-x snap-mandatory`}
+    >
+      {mugsItems.map((item) => (
         <HeroListItem key={item.id} mugItem={item} />
-      )) }
+      ))}
     </ul>
   );
-}
+};
